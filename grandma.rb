492df838f1c0d5ab6e501f91fa894a524893117b
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 14:12:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,12 @@
 def speak_to_grandma(speech)
   if (speech==speech.upcase) && (speech=='I LOVE YOU GRANDMA!')
     puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif speech==speech.upcase
-    puts "NO, ?NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
