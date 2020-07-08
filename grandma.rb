af551807a7a757bc6f4d9c64dcad51d3d1c90ed6
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 14:09:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,7 +15,7 @@ def speak_to_grandma(speech)
   if (speech==speech.upcase) && (speech=='I LOVE YOU GRANDMA!')
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif speech==speech.upcase
-    puts "NO, ?OT SINCE 1938!" 
+    puts "NO, ?OT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
